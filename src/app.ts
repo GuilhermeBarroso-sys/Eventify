46570164b98b4a7833b2139ab6d81a4b7ec33003
guilhermebarroso-sys <guibarrosodeoliveira5@gmail.com>
2023-08-17 23:17:34 -0300

    feat: :sparkles: Installing cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import cookieSession from "cookie-session";
+import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import { NotFoundError } from "./errors/NotFoundError";
 import { errorHandler } from "./middlewares/errorHandler";
 const app = express()
-// app.use(cors())
+app.use(cors())
 app.set("trust proxy", true)
 app.use(express.json())
 app.use(cookieSession({
